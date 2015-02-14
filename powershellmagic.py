a49e09ab00311a08296ce830443cc1b06338a502
Eric Galloway <ericgalloway@gmail.com>
2015-02-13 19:35:21 -0800

    Update comments

--- powershellmagic.py
+++ powershellmagic.py
@@ -16,9 +16,9 @@ from IPython.core.magic_arguments import (
 @magics_class
 class PowerShellMagics(Magics):
     """IPython magics class for Windows PowerShell.
-
-    This class is patterned after IPython.core.magics.script.ScriptMagics.
     """
+    # This class is patterned after
+    # IPython.core.magics.script.ScriptMagics.
 
     def __init__(self, shell=None):
         super(PowerShellMagics, self).__init__(shell=shell)
@@ -31,22 +31,35 @@ class PowerShellMagics(Magics):
         os.remove(self._input_file_name)
 
     @magic_arguments()
-    @argument('--out', type=str)
-    @argument('--err', type=str)
+    @argument(
+        '--out',
+        type=str,
+        help="Redirect stdout to a variable."
+        )
+    @argument(
+        '--err',
+        type=str,
+        help="Redirect stderr to a variable."
+        )
     @cell_magic
     def powershell(self, line, cell):
-        """Execute a cell body using Powershell.
+        """Execute a cell written in PowerShell by spawning a process
+        that invokes the command:
 
-        This function is patterned after
-        IPython.core.magics.ScriptMagics.shebang.
+           PowerShell -ExecutionPolicy RemoteSigned -File tempfile.ps1
+
+        where the argument to '-File' is a file that contains the contents
+        of the cell.
         """
+        # This function is patterned after
+        # IPython.core.magics.ScriptMagics.shebang.
 
         args = parse_argstring(self.powershell, line)
 
         with open(self._input_file_name, mode='w') as f:
             f.write(cell)
 
-        cmd = 'powershell -ExecutionPolicy RemoteSigned -File {}\r\n'
+        cmd = 'PowerShell -ExecutionPolicy RemoteSigned -File {}\r\n'
         cmd = cmd.format(self._input_file_name)
 
         p = Popen(cmd.split(), stdout=PIPE, stderr=PIPE, stdin=PIPE)
